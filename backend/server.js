cb2e892b3ac9f3caacaa978f1de5cc051cc17aad
a.doudement <a.doudement@groupeonepoint.com>
2023-10-02 10:27:14 +0200

    feat: configure server js

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,47 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
 
-app.use(cors(
-    {
-      credentials: true,
-      origin: ["http://localhost:4200"],
-    }));
+var corsOptions = {
+  origin: "http://localhost:4200"
+};
 
+app.use(cors(
+    corsOptions));
+
+// parse request of content type - application / json
+app.use(bodyParser.json());
+
+// parse request of content type - application/x-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+// database
 const db = require("./app/models");
-db.sequelize.sync()
+
+// force: true will drop the table if it already exists
+db.sequelize.sync({force: true})
 .then(() => {
-  console.log("Synced db.");
+  console.log("Drop et Synced database with force true");
 })
 .catch((err) => {
   console.log("Failed to sync db: " + err.message);
 });
 
-// parse requests of content-type - application/json
-app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Coucou" });
+  res.json({message: "Coucou"});
 });
+
+//routes
+require('./app/routes/user.routes');
+require('./app/routes/auth.routes');
 
 // set port, listen for requests
 const PORT = process.env.PORT || 4200;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+// function initial() ?
