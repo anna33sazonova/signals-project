185a417c8fa0c8a08d8cd214c92c61665323dfb5
a.doudement <a.doudement@groupeonepoint.com>
2023-10-03 12:07:37 +0200

    feat: remove dependencies, fix config files

--- backend/app/routes/auth.routes.js
+++ backend/app/routes/auth.routes.js
@@ -5,10 +5,11 @@ module.exports = function(app){
   app.use(function(req, res, next){
     res.header(
         "Access-Control-Allow-Headers",
-        "x-access-token, Origin, Content-Type, Accept"
+        "Origin, Content-Type, Accept"
     );
     next();
   });
+
   app.post(
       "api/auth/signup",
       [
@@ -16,5 +17,8 @@ module.exports = function(app){
       ],
       controller.singup
   );
+
   app.post("app/auth/signup", controller.singin);
+
+  app.post("/api/auth/signout", controller.signout);
 };
