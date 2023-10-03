2b2ecc49e1e1b103d30cbb51fc20e79a8d40a24e
a.doudement <a.doudement@groupeonepoint.com>
2023-10-03 16:29:43 +0200

    feat: fix port, change auth service

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cookieSession = require("cookie-session");
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:4200"
+  origin: "http://localhost:8080"
 };
 
 app.use(cors(
@@ -48,7 +48,7 @@ require('./app/routes/user.routes')(app);
 require('./app/routes/auth.routes')(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 4200;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
