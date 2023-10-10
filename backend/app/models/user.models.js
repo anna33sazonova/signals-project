d3e009d42f1dbd1906c7c417740fe772e41ce80a
a.doudement <a.doudement@groupeonepoint.com>
2023-10-10 09:53:59 +0200

    feat: change model user

--- backend/app/models/user.models.js
+++ backend/app/models/user.models.js
@@ -1,13 +1,17 @@
 module.exports = (sequelize, Sequelize) => {
   const User = sequelize.define("users", {
     username: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING(100),
+      required: true
     },
     email: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING(50),
+      required: true,
+      unique: true
     },
     password: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING(),
+      required: true
     }
   });
   return User;
